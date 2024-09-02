af0fa37a81f3debf34be6b5e74ce860325fd4e91
lx <3069673268@qq.com>
2024-09-02 20:10:35 +0800

    二级整体业务认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Category from '@/views/Category/index.vue'
 import Home from '@/views/Home/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,6 +17,10 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
         },
         {
           path: '',
