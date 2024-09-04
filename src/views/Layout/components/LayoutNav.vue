5e101002ae7bd1fb19674e52c99baf00d5e3a5f4
lx <3069673268@qq.com>
2024-09-04 22:10:21 +0800

    我的订单

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -37,7 +37,11 @@ const comfirm = () => {
             </el-popconfirm>
           </li>
           <li><a href="javascript:;">我的订单</a></li>
-          <li><a href="javascript:;">会员中心</a></li>
+          <li>
+            <a href="javascript:;" @click="() => $router.push('/member')"
+              >会员中心</a
+            >
+          </li>
         </template>
         <template v-else>
           <li>
