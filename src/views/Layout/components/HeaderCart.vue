1782b5c92efa8f7a31d1da99480bb2c232a4b0bf
lx <3069673268@qq.com>
2024-09-04 16:08:54 +0800

    结算页路由配置和基础数据渲染

--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { useRouter } from 'vue-router'
 import { useCartStore } from '@/stores/cartStore'
+const router = useRouter()
 const cartStore = useCartStore()
 </script>
 
@@ -35,7 +37,12 @@ const cartStore = useCartStore()
           <p>共 {{ cartStore.allCount }} 件商品</p>
           <p>&yen; {{ cartStore.allPrice.toFixed(2) }}</p>
         </div>
-        <el-button size="large" type="primary">去购物车结算</el-button>
+        <el-button
+          size="large"
+          type="primary"
+          @click="() => router.push('/cartlist')"
+          >去购物车结算</el-button
+        >
       </div>
     </div>
   </div>
