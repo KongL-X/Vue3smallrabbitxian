13ecc0e7cc6441a8d57d352388dfbf8d3f8f58ac
lx <3069673268@qq.com>
2024-09-04 12:57:14 +0800

    接口购物车加入与删除

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -3,34 +3,54 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { computed } from 'vue'
+import { insertCartAPI, findNewCartListAPI, delCartAPI } from '@/apis/cart'
+import { useUserStore } from './userStore'
 export const useCartStore = defineStore(
   'cart',
   () => {
+    const userStore = useUserStore()
+    const isLogin = computed(() => userStore.userInfo.token)
     // 1. 定义state - cartList
     const cartList = ref([])
     // 2. 定义action - addCart
-    const addCart = (goods) => {
+    const addCart = async (goods) => {
       console.log('添加', goods)
-      // 添加购物车操作
-      // 已添加过 - count + 新增数量
-      // 没有添加过 - 直接push
-      // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
-      const item = cartList.value.find((item) => goods.skuId === item.skuId)
-      if (item) {
-        // 找到了
-        item.count += goods.count
+      const { skuId, count } = goods
+      // 登录
+      if (isLogin.value) {
+        // 登录之后的加入购车逻辑
+        await insertCartAPI({ skuId, count })
+        updataNewList()
       } else {
-        // 没找到
-        cartList.value.push(goods)
+        // 添加购物车操作
+        // 已添加过 - count + 新增数量
+        // 没有添加过 - 直接push
+        // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
+
+        const item = cartList.value.find((item) => goods.skuId === item.skuId)
+        if (item) {
+          // 找到了
+          item.count += goods.count
+        } else {
+          // 没找到
+          cartList.value.push(goods)
+        }
       }
     }
     // 删除购物车
+    // 删除购物车
     const delCart = async (skuId) => {
-      // 思路：
-      // 1. 找到要删除项的下标值 - splice
-      // 2. 使用数组的过滤方法 - filter
-      const idx = cartList.value.findIndex((item) => skuId === item.skuId)
-      cartList.value.splice(idx, 1)
+      if (isLogin.value) {
+        // 调用接口实现接口购物车中的删除功能
+        await delCartAPI([skuId])
+        updataNewList()
+      } else {
+        // 思路：
+        // 1. 找到要删除项的下标值 - splice
+        // 2. 使用数组的过滤方法 - filter
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+      }
     }
     //计算总数量
     const allCount = computed(() =>
@@ -40,6 +60,11 @@ export const useCartStore = defineStore(
     const allPrice = computed(() =>
       cartList.value.reduce((a, c) => a + c.count * c.price, 0)
     )
+    //获取最新的购物车列表action
+    const updataNewList = async () => {
+      const res = await findNewCartListAPI()
+      cartList.value = res.result
+    }
     // 单选功能
     const singleCheck = (skuId, selected) => {
       // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
@@ -54,6 +79,18 @@ export const useCartStore = defineStore(
 
     // 是否全选计算属性
     const isAll = computed(() => cartList.value.every((item) => item.selected))
+    // 3. 已选择数量
+    const selectedCount = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count, 0)
+    )
+    // 4. 已选择商品价钱合计
+    const selectedPrice = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count * c.price, 0)
+    )
     return {
       cartList,
       addCart,
@@ -62,7 +99,9 @@ export const useCartStore = defineStore(
       allPrice,
       singleCheck,
       isAll,
-      allCheck
+      allCheck,
+      selectedCount,
+      selectedPrice
     }
   },
   {
